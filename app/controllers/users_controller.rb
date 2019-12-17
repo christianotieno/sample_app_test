eebeac6c7f41bbdd2478d7dff737c964b951f0bb
ChristianOtieno <cgao273@gmail.com>
2019-12-17 20:51:14 +0300

    The user create action with a save and a redirect

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,18 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
-  
   def show
     @user = User.find(params[:id])
   end
-  
+
   def new
     @user = User.new
   end
+
   def create
     @user = User.new(user_params)
     if @user.save
-      # Handle a successful save.
+      redirect_to @user
     else
       render 'new'
     end
@@ -20,8 +20,8 @@ class UsersController < ApplicationController
 
   private
 
-    def user_params
-      params.require(:user).permit(:name, :email, :password,
-                                   :password_confirmation)
-    end
+  def user_params
+    params.require(:user).permit(:name, :email, :password,
+                                 :password_confirmation)
+  end
 end
