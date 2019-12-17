6760f32ebfdc41758479b6410785df457a389a26
ChristianOtieno <cgao273@gmail.com>
2019-12-17 23:45:37 +0300

    Configuring the database for production

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,12 +4,8 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
   def setup
-<<<<<<< HEAD
-    @user = User.new(name: 'Example User', email: 'user@example.com', password: 'foobar', password_confirmation: 'foobar')
-=======
     @user = User.new(name: 'Example User', email: 'user@example.com',
                      password: 'foobar', password_confirmation: 'foobar')
->>>>>>> sign-up
   end
 
   test 'should be valid' do
