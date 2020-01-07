f0b97c2a8a14b11ffa2d0c4344bf9f2cfd3aa9d1
ChristianOtieno <cgao273@gmail.com>
2020-01-07 22:40:27 +0300

    Testing that the admin attribute is forbidden

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -17,4 +17,22 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     get users_path
     assert_redirected_to login_url
   end
+
+  test 'should redirect update when not logged in' do
+    patch user_path(@user), params: { user: { name: @user.name,
+                                              email: @user.email } }
+    assert_not flash.empty?
+    assert_redirected_to login_url
+  end
+
+  test 'should not allow the admin attribute to be edited via the web' do
+    log_in_as(@other_user)
+    assert_not @other_user.admin?
+    patch user_path(@other_user), params: {
+      user: { password: FILL_IN,
+              password_confirmation: FILL_IN,
+              admin: FILL_IN }
+    }
+    assert_not @other_user.FILL_IN.admin?
+  end
 end
