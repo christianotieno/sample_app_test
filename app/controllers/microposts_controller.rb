a2a94f697c62a00bed2bd7b5462ef45806804283
ChristianOtieno <cgao273@gmail.com>
2020-01-22 21:00:56 +0300

    The Microposts controller create action

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -3,7 +3,21 @@
 class MicropostsController < ApplicationController
   before_action :logged_in_user, only: %i[create destroy]
 
-  def create; end
+  def create
+    @microspost = current_user.microposts.build(microspost_params)
+    if @microspost.after_save
+      flash[:success] = 'Micropost created!'
+      redirect_to root_url
+    else
+      render 'static_pages/home'
+    end
+  end
 
   def destroy; end
+
+  private
+
+  def microspost_params
+    params.require(:micropost).permit(:content)
+  end
 end
