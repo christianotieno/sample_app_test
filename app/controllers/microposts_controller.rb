9c44c4163ecdd6ec54a38d6cb5c87ae6469b6e9a
ChristianOtieno <cgao273@gmail.com>
2020-01-22 23:23:51 +0300

    Adding a micropost with a different owner

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -2,6 +2,7 @@
 
 class MicropostsController < ApplicationController
   before_action :logged_in_user, only: %i[create destroy]
+  before_action :current_user, only: :destroy
 
   def create
     @microspost = current_user.microposts.build(microspost_params)
@@ -13,11 +14,20 @@ class MicropostsController < ApplicationController
     end
   end
 
-  def destroy; end
+  def destroy
+    @microspost.destroy
+    flash[:success] = 'Micropost deleted'
+    redirect_to request.referrer || root_url
+  end
 
   private
 
   def microspost_params
     params.require(:micropost).permit(:content)
+  end
+
+  def correct_user
+    @micropost = current_user.microposts.find_by(id: params[:id])
+    redirect_to root_url if @micropost.nil?
   end
 end
